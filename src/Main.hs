1027a20a480a80ce15ae1566420b051be1ea579c
Shaun Sharples <shaun.sharples@gmail.com>
2025-03-24 11:31:16 +0200

    Enhance TigerBeetle internal structures by adding Show instances for AccountFlags, TransferFlags, CreateAccountResult, CreateTransferResult, AccountFilterFlags, QueryFilterFlags, Operation, PacketStatus, InitStatus, ClientStatus, RegisterLogCallbackStatus, and LogLevel. Introduce utility functions for flag name retrieval and improve flag representation in string format. Update Main.hs to demonstrate the usage of createAccounts and createTransfers functions.

--- src/Main.hs
+++ src/Main.hs
@@ -15,11 +15,11 @@ main = do
       transfer = Transfer 1 1 2 100 0 0 0 0 0 1 1 0 0
   withClient 0 "127.0.0.1:3001" $ \client -> do
     putStrLn "Client created successfully"
-    -- result1 <- createAccounts client [account1, account2]
-    -- putStrLn $ "Result: " ++ show result1
+    result1 <- createAccounts client [account1, account2]
+    putStrLn $ "Result: " ++ show result1
     result2 <- lookupAccounts client [1, 2]
     putStrLn $ "Result: " ++ show result2
-    -- result3 <- createTransfers client [transfer]
-    -- putStrLn $ "Result: " ++ show result3
+    result3 <- createTransfers client [transfer]
+    putStrLn $ "Result: " ++ show result3
     result4 <- getAccountTransfers client filter1
     putStrLn $ "Result: " ++ show result4
